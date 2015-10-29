23ddc014a965578c6ddc1aa92407c67f8afe92be
Freja Thoresen <thoresen@hep01.cluster>
2015-10-29 15:10:39 +0100

    option to choose grid run or local run

--- theSearch/util/RunSearch.cxx
+++ theSearch/util/RunSearch.cxx
@@ -4,6 +4,7 @@
 #include "SampleHandler/ToolsDiscovery.h"
 #include "EventLoop/Job.h"
 #include "EventLoop/DirectDriver.h"
+#include "EventLoopGrid/PrunDriver.h"
 #include "SampleHandler/DiskListLocal.h"
 #include <TSystem.h>
 #include "SampleHandler/ScanDir.h"
@@ -14,8 +15,8 @@
 
 using namespace std;
 
-int main( int argc, char* argv[] ) {
-
+int main( int argc, char* argv[]) {
+char gridon = '1';
     // Take the submit directory from the input if provided:
     std::string submitDir = "submitDir";
     if( argc > 1 ) submitDir = argv[ 1 ];
@@ -27,12 +28,18 @@ int main( int argc, char* argv[] ) {
     SH::SampleHandler sh;
 
     // use SampleHandler to scan all of the subdirectories of a directory for particular MC single file:
-    //const char* inputFilePath = gSystem->ExpandPathName ("/hep/storage/thoresen/mc15_13TeV.361610.PowhegPy8EG_CT10nloME_AZNLOCTEQ6L1_ZZqqll_mqq20mll20.merge.DAOD_HIGG2D4.e4054_s2608_s2183_r6869_r6282_p2419/");
-    //SH::ScanDir().filePattern("DAOD_HIGG2D4.06464156.*").scan(sh, inputFilePath);
 
 
     // for grid
-    SH::scanDQ2 (sh, "mc15_13TeV.361610.PowhegPy8EG_CT10nloME_AZNLOCTEQ6L1_ZZqqll_mqq20mll20.merge.DAOD_HIGG2D4.e4054_s2608_s2183_r6869_r6282_p2419/");
+    if (*argv[2] == gridon) {
+        SH::scanDQ2(sh,
+                    "mc15_13TeV.361610.PowhegPy8EG_CT10nloME_AZNLOCTEQ6L1_ZZqqll_mqq20mll20.merge.DAOD_HIGG2D4.e4054_s2608_s2183_r6869_r6282_p2419/");
+    }
+    else {
+        const char *inputFilePath = gSystem->ExpandPathName(
+                "/hep/storage/thoresen/mc15_13TeV.361610.PowhegPy8EG_CT10nloME_AZNLOCTEQ6L1_ZZqqll_mqq20mll20.merge.DAOD_HIGG2D4.e4054_s2608_s2183_r6869_r6282_p2419/");
+        SH::ScanDir().filePattern("DAOD_HIGG2D4.06464156.*").scan(sh, inputFilePath);
+    }
 
 
     // Set the name of the input TTree. It's always "CollectionTree"
@@ -59,16 +66,22 @@ int main( int argc, char* argv[] ) {
     job.algsAdd (ntuple);
     alg->outputName = "search_out"; // give the name of the output to our algorithm
 
-    // Run the job using the local/direct driver:
-    //EL::DirectDriver driver;
 
     // for grid
-    EL::PrunDriver driver;
-    driver.options()->setString("nc_outputSampleName", "user.fthorese.Searching_for_Z.TEST.%in:name[2]%.%in:name[6]%");
+    if (*argv[2] == gridon) {
+        EL::PrunDriver driver;
+        driver.options()->setString("nc_outputSampleName",
+                                    "user.fthorese.Searching_for_Z.TEST.%in:name[2]%.%in:name[6]%");
+        driver.submit( job, submitDir );
+    }
+    else {
+        EL::DirectDriver driver;
+        driver.options()->setString("nc_outputSampleName",
+                                    "user.fthorese.Searching_for_Z.TEST.%in:name[2]%.%in:name[6]%");
+        driver.submit( job, submitDir );
+    }
 
-    //driver.options()->setString("nc_outputSampleName", "user.fthorese.test.%in:name[2]%.%in:name[6]%");
 
-    driver.submit( job, submitDir );
 
     return 0;
 }
